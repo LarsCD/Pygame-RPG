1d6985fd11963e07233476cd4d1add1b40b612c0
LarsCD <larscdijkgraaf@gmail.com>
2023-05-17 00:21:47 +0200

    Initial commit

---
+++ core.py
@@ -0,0 +1,9 @@
+
+class Core:
+    def __init__(self):
+        self.run_game = True
+
+    def run_game(self):
+        while self.run_game:
+            # update game
+            print('running_game')
---
+++ main.py
@@ -0,0 +1,11 @@
+from core import Core
+
+class Main:
+    def main(self):
+        core = Core
+        core.run_game()
+
+if __name__ == '__main__':
+    main =
+    Main.main()
+
