41a940ab9290c84997a70c7f6dfca1cf105b346c
LarsCD <larscdijkgraaf@gmail.com>
2023-05-19 19:49:18 +0200

    working on pygame loop

--- game_loop.py
+++ game_loop.py
@@ -6,28 +6,55 @@ class Game_Loop:
         self.Game_Main = Game_Main
         self.run_display = True
 
+        # SCENE COLORS
+        self.default_bg_color = self.Game_Main.default_bg_color
+        self.bg_color = self.Game_Main.bg_color
+
+        # SCENE LABELS
+        self.static_text_lables = []
+
+
     def main_loop(self):
         # MAIN GAME LOOP
-        # EVERYTHING GAME RELATED GOES HERE
+        # EVERYTHING GAME LOOP RELATED GOES HERE
         self.run_display = True
-        play_label = Lable('PLAYING GAME', 40, 'white', 'gray', 'green',
-                           (self.Game_Main.DISPLAY_WIDTH / 2, self.Game_Main.DISPLAY_HEIGHT / 2),
-                           is_centered=True, is_clickable=False)
+        self.build_static_text_lables()
         back_label = Lable('MAIN MENU', 20, 'white', 'gray', 'red',
                                         ((self.Game_Main.DISPLAY_WIDTH / 2), (self.Game_Main.DISPLAY_HEIGHT / 2) + 140),
                                         is_centered=True, is_clickable=True)
+
         while self.run_display:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.run_display = False
+            self.check_quit_event()
 
-            gray = (64, 64, 64)
-            self.Game_Main.window.fill(gray)
+            self.set_background_color()
+            self.draw_static_text_labels()
 
-            play_label.draw_text(self.Game_Main.window)
             if back_label.draw_text(self.Game_Main.window):
                 self.run_display = False
 
             pygame.display.update()
             self.Game_Main.clock.tick(self.Game_Main.fps)
 
+
+
+    def set_background_color(self):
+        if self.bg_color != None:
+            self.Game_Main.window.fill(self.bg_color)
+        else:
+            self.Game_Main.window.fill(self.default_bg_color)
+
+    def build_static_text_lables(self):
+        play_label = Lable('PLAYING GAME', 40, 'white', 'gray', 'green',
+                           (self.Game_Main.DISPLAY_WIDTH / 2, self.Game_Main.DISPLAY_HEIGHT / 2),
+                           is_centered=True, is_clickable=False)
+        # package labels
+        self.static_text_lables.append(play_label)
+
+    def draw_static_text_labels(self):
+        for label in self.static_text_lables:
+            label.draw_text(self.Game_Main.window)
+
+    def check_quit_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.run_display = False
--- game_main.py
+++ game_main.py
@@ -2,9 +2,10 @@ import pygame
 from Pygame_Tutorials.custom_pygame_assets import Lable
 from game_loop import Game_Loop
 
+
 class Game_Main:
     def __init__(self, resolution: tuple, default_font: str, bold_font: str, lable_col: tuple,
-                 lable_click_col: tuple, lable_hover_col: tuple):
+                 lable_click_col: tuple, lable_hover_col: tuple, bg_color: tuple=None):
         # PYGAME
         pygame.init()
         self.clock = pygame.time.Clock()
@@ -20,20 +21,28 @@ class Game_Main:
         self.default_font = default_font
         self.fps = 60
 
-        # DEFAULT TEXT PARAMETERS
+        # DEFAULT TEXT LABLE PARAMETERS
         self.bold_font = bold_font
         self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
         self.lable_col = lable_col
         self.lable_click_col = lable_click_col
         self.lable_hover_col = lable_hover_col
 
-        # OPTIONS
+        # SCENE COLORS
+        self.default_bg_color = (64, 64, 64)
+        self.bg_color = bg_color
+
+        # SCENE LABELS
+        self.static_text_lables = []
+
+        # MAIN GAME LOOP
         self.Game_Loop = Game_Loop(self)
 
 
     def main_menu_loop(self):
-        text_button = Lable('RPG3: MAIN MENU', 25, self.lable_col, self.lable_click_col, self.lable_hover_col, (2, 2),
-                            is_clickable=False)
+        # setup
+        self.build_static_text_lables()
+
         play_label = Lable('PLAY GAME', 60, self.lable_col, self.lable_click_col, self.lable_hover_col,
                               ((self.DISPLAY_WIDTH / 2), (self.DISPLAY_HEIGHT / 2)-30), is_centered=True)
         options_label = Lable('OPTIONS', 40, self.lable_col, self.lable_click_col, self.lable_hover_col,
@@ -41,13 +50,12 @@ class Game_Main:
         about_label = Lable('ABOUT', 40, self.lable_col, self.lable_click_col, self.lable_hover_col,
                               ((self.DISPLAY_WIDTH / 2), (self.DISPLAY_HEIGHT / 2) + 105), is_centered=True)
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running, self.playing = False, False
-            gray = (64, 64, 64)
-            self.window.fill(gray)
+            self.check_quit_event()
 
-            text_button.draw_text(self.window)
+            self.set_background_color()
+            self.draw_static_text_labels()
+
+
             if play_label.draw_text(self.window):
                 # click play
                 # RUN MAIN GAME LOOP
@@ -63,6 +71,33 @@ class Game_Main:
             self.window.blit(self.window, (0,0))
             pygame.display.update()
             self.clock.tick(self.fps)
+
+
+    def set_background_color(self):
+        if self.bg_color != None:
+            self.window.fill(self.bg_color)
+        else:
+            self.window.fill(self.default_bg_color)
+
+    def build_static_text_lables(self):
+        title_label = Lable('RPG3: MAIN MENU', 25, self.lable_col, self.lable_click_col, self.lable_hover_col, (2, 2),
+                            is_clickable=False)
+        # package labels
+        self.static_text_lables.append(title_label)
+
+    def draw_static_text_labels(self):
+        for label in self.static_text_lables:
+            label.draw_text(self.window)
+
+    def check_quit_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+
+class Options:
+    def __init__(self):
+        pass
+
 
 
 
